f580aed6b7b72efa3035038692667c6820bf6b53
Victor Matos <antoniovitomatos@gmail.com>
2022-08-14 23:09:11 -0300

    bugfix

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import errorHandler from './middlewares/errorHandlerMiddleware.js';
 const app = express();
 app.use(cookieParser());
 app.use(json());
-app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://project17-linkedir-front-430paj3fr-nikolasrr.vercel.app"] }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://daily-news-nine.vercel.app", "*"] }));
 app.use(router);
 app.use(errorHandler);
 
